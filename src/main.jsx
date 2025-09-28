d7450860999b42e6d0fc7ea4a8b96c2762d4e86f
meshacktimothy <137101889+meshacktimothy@users.noreply.github.com>
2025-09-28 05:59:57 +0100

    index.css main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-// import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
